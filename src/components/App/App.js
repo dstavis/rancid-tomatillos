6a6a7055f70875eb76f329cc7099e6fa5810d5d1
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-08-25 20:17:06 -0600

    App display movie cards with image title and rating - Ready for MovieDetail component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,64 @@
-import logo from '../../assets/logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CardContainer from "../CardContainer/CardContainer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      loading: false,  
+      movies: [],
+      movieClicked: undefined
+    }
+  }
+
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+      .then(response => response.json())
+      .then(response => {
+        const movieData = response.movies
+        this.setState({
+          loading: false,
+          movies: movieData
+        })
+      })
+  }
+
+  handleClick = (id) => {
+    this.setState({
+      movieClicked: id
+    })
+  }
+
+  closeMovieDetails = () => {
+    this.setState({
+      movieClicked: undefined
+    })
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <h1>Rancid Tomatillos</h1>
+        {this.state.loading ? <h1>Loading... </h1> :
+          (!this.state.movieClicked &&
+           <CardContainer 
+             allMovies={this.state.movies}
+             handleClick={this.handleClick}
+           />
+         )
+        }
+        {/* {this.state.movieClicked && 
+        <MovieDetails 
+          id={this.state.movieClicked}
+          closeMovieDetails={this.closeMovieDetails}
+        />
+        } */}
+      </main>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const MovieCard = ({ id, title, poster, handleClick, averageRating}) => {
+    const movieCardStyles ={
+        width: "20%"
+    }
+
+    const imgStyle = {
+        width: "100%"
+    }
+    
+    return (
+        <div style={movieCardStyles} className="movie-card">
+            <img style={imgStyle} src={poster} alt="{title} movie poster"/>
+            <h3>{title}</h3>
+            <p> Rating: {Math.round(averageRating)}/10 </p>
+            <button onClick={() => handleClick(id)}>Movie Details</button>
+        </div>
+    )
+}
+
+export default MovieCard
