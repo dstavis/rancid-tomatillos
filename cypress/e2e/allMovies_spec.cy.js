13da8ba27c50b05b28256409cb47079b6bd2bb3a
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-09-01 16:55:51 -0600

    Refactor movie detail tests. Begin refactor of allMovies test

--- cypress/e2e/allMovies_spec.cy.js
+++ cypress/e2e/allMovies_spec.cy.js
@@ -1,13 +1,7 @@
 describe('RANCID', () => {
   beforeEach( () => {
-    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {statusCode: 201, body: {
-      id: 694919,
-      poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-      backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-      title: "Money Plane",
-      average_rating: 6.875,
-      release_date: "2020-09-29"
-    }})
+    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {statusCode: 201})
+    console.log(cy.fixture("example"))
     cy.visit('localhost:3000')
   })
 
@@ -19,11 +13,17 @@ describe('RANCID', () => {
   //   cy.get('div[class="movie-card]').should()
   // })
 
-  it('should try to get movies data', () => {
-    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {statusCode: 201})
-  })
+  it('should show each movies title, rating, and poster', () => {
 
-  it('should try to get movies data', () => {
+    cy.visit("localhost:3000/").get('a.movie-card')
+    cy.get('div.movie-card-container').should("", "Money Plane") 
+    
+    cy.get('h1.title').contains("lalala")
+    cy.get('h1.rating').contains("Rating: ")
+
+    
+
+
   })
 
 
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -9,10 +9,10 @@ const MovieCard = ({ id, title, poster, averageRating}) => {
 
   return (
     <Link to={"/movie_details/" + id} className="movie-card">
-        <div style={backgroundPoster} className="background-styles">
+        <div style={backgroundPoster} className="background-style">
           <div className="overlay overlay-style">
-            <h1>{title}</h1>
-            <h1> Rating: {Math.round(averageRating)}/10 </h1>
+            <h1 className="title">{title}</h1>
+            <h1 className="rating"> Rating: {Math.round(averageRating)}/10 </h1>
           </div>
         </div>
     </Link>
