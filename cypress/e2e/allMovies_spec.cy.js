b34fb5c1901ed62d6d574a0cb61a4fa1a8c8e0f9
David Stavis <davidstavis@Davids-MacBook-Pro.local>
2022-09-01 14:09:02 -0600

    Complete title test

--- cypress/e2e/allMovies_spec.cy.js
+++ cypress/e2e/allMovies_spec.cy.js
@@ -1,5 +1,13 @@
 describe('RANCID', () => {
   beforeEach( () => {
+    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {statusCode: 201, body: {
+      id: 694919,
+      poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+      backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+      title: "Money Plane",
+      average_rating: 6.875,
+      release_date: "2020-09-29"
+    }})
     cy.visit('localhost:3000')
   })
 
@@ -13,6 +21,9 @@ describe('RANCID', () => {
 
   it('should try to get movies data', () => {
     cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {statusCode: 201})
+  })
+
+  it('should try to get movies data', () => {
   })
 
 
--- cypress/e2e/movieDetail_spec.cy.js
+++ cypress/e2e/movieDetail_spec.cy.js
@@ -1,12 +1,34 @@
+// look into fixtures; but sample data in a fixture and import it into the test so that the intercept can use it
+
+const exampleMovieJSON = {
+  "movie": {
+  "id": 694919,
+  "title": "Money Plane",
+  "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+  "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+  "release_date": "2020-09-29",
+  "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+  "genres": [
+  "Action"
+  ],
+  "budget": 0,
+  "revenue": 0,
+  "runtime": 82,
+  "tagline": "",
+  "average_rating": 6.875
+  }
+}
+
 describe('RANCID', () => {
   beforeEach( () => {
-    
+    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919", {statusCode: 201, body: exampleMovieJSON
+  })
   })
 
   it('should have a link to all movies', () => {
-    cy.visit("localhost:3000").get('div[class="movie-card"]').first().click()
+    cy.visit("localhost:3000").get('div.movie-card').first().click()
     cy.contains("h2", "All Movies")
-   })
+  })
 
   it('should load the movie details page when the user clicks on a movie card', () => {
     cy.visit("localhost:3000").get('div[class="movie-card"]').first().click().url().should("include", "/movie_details")
@@ -23,16 +45,8 @@ describe('RANCID', () => {
 
   it('should try to get data for one movie', () => {
 
-    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919", {statusCode: 201, body: {
-      id: 694919,
-      poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-      backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-      title: "Money Plane",
-      average_rating: 6.875,
-      release_date: "2020-09-29"
-    }})
-    cy.visit("localhost:3000").get('div[class="movie-card"]').first().click()
-    // cy.get('img[class="poster"]').should().not().eq(undefined)
+    cy.visit("localhost:3000/movies/").get('div.movie-card').first().click()
+    cy.get('div.movie-title').contains("h1", "Money Plane")
 
   })
 })
