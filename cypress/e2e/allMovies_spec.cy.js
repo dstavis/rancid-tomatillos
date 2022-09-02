067c71b8e0994be90e6ddbe6acaad88d3109ee1a
David Stavis <davidstavis@Davids-MacBook-Pro.local>
2022-09-02 10:11:23 -0600

    Get fixture returning 5 example movie datas and causing the cypress version of the site to properly display movie cards from the stubbed fixture data

--- cypress/e2e/allMovies_spec.cy.js
+++ cypress/e2e/allMovies_spec.cy.js
@@ -1,25 +1,34 @@
 describe('RANCID', () => {
   beforeEach( () => {
-    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {statusCode: 201})
-    console.log(cy.fixture("example"))
-    cy.visit('localhost:3000')
+    let movieData;
+    cy.fixture("movies").then( (fixtureJSON) => {
+      movieData = fixtureJSON
+      cy.intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies/", { statusCode: 201, body: movieData })
+    })
+    cy.visit('http://localhost:3000/')
   })
+  
+  // it('should load the fixture', () => {
+  //   cy.fixture("movies").then( (movieData) => {
+  //     expect(694919).to.equal(movieData.movies[0].id)
+  //   })
+  // })
 
   it('should load the home page', () => {
     cy.contains('h1', 'Rancid')
   })
-  
-  // it('should load all the movie cards', () => {
-  //   cy.get('div[class="movie-card]').should()
-  // })
+
+  it('should load at least one movie card', () => {
+    cy.visit("localhost:3000/").get('a.movie-card')
+  })
 
   it('should show each movies title, rating, and poster', () => {
 
     cy.visit("localhost:3000/").get('a.movie-card')
-    cy.get('div.movie-card-container').should("", "Money Plane") 
+    // cy.get('div.movie-card-container').contain("", "Money Plane") 
     
-    cy.get('h1.title').contains("lalala")
-    cy.get('h1.rating').contains("Rating: ")
+    // cy.get('h1.title').contains("lalala")
+    // cy.get('h1.rating').contains("Rating: ")
 
     
 
